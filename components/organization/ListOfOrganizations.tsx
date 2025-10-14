7d24b7d0eb9c49e312a450f04fcc1c15a0550c25
William Lima <willy.lima89@gmail.com>
2025-10-14 19:49:53 -0100

    hide main org from portal

--- components/organization/ListOfOrganizations.tsx
+++ components/organization/ListOfOrganizations.tsx
@@ -12,14 +12,10 @@ export default function ListOfOrgs({
   searchString: string;
   miniSearch: MiniSearch<any>;
 }) {
-  //We only do search when the string is different from ""
-  //Because otherwise minichsearch gives an empty list
-
-  //We also only hide the element instead of recreating
-  //Because thats faster especially to the display image
+  const mainOrg = process.env.NEXT_PUBLIC_ORG;
   return (
     <section className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {orgs.map((org) => (
+      {orgs.filter((o)=>o.name !== mainOrg).map((org) => (
         <div
           className={`col-span-1 ${
             searchString !== "" &&
