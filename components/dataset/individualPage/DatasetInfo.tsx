7876f4eb48426205bf3ffa3ba394bf5e26ed1182
William Lima <willy.lima89@gmail.com>
2025-10-16 11:00:16 -0100

    fix tabs

--- components/dataset/individualPage/DatasetInfo.tsx
+++ components/dataset/individualPage/DatasetInfo.tsx
@@ -44,7 +44,7 @@ export default function DatasetInfo({
   }, [dataset.notes]);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-fit">
       <div className="flex flex-col gap-y-3">
         {dataset.type === "visualization" && !!dataset.external_url && (
           <a
@@ -134,14 +134,14 @@ export default function DatasetInfo({
         </span>
       </div>
       <div className="py-4 my-4 border-y">
-        <p
+        <div
           ref={textRef}
           className={`text-sm font-normal text-stone-500 transition-all ${
             !showFullDescription ? "line-clamp-4" : ""
           }`}
         >
           <MarkdownRenderer content={description}/>
-        </p>
+        </div>
         {isTruncated && (
           <button
             onClick={() => setShowFullDescription(!showFullDescription)}
