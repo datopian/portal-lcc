0bb050324f062358e22b6756488548e3f05a76c3
steveoni <steohenoni2@gmail.com>
2025-11-04 11:41:47 +0100

    add googleanalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,14 +13,33 @@ import ThemeProvider from "../components/theme/theme-provider";
 
 import { Open_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
   variable: "--font-open-sans"
 });
 
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID ?? '';
+
+const handleRouteChange = (url: string) => {
+  (window as any).gtag?.("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   const theme = pageProps.theme || "lighter";
+    const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
     <div className={cn(openSans.variable, "font-sans")}>
       <ThemeProvider themeName={theme}>
